2d2cce48e6df47dd0531d5dcd597872c2aa637c6
Aed-Matthew <abridgedun@gmail.com>
2016-11-17 22:00:20 -0800

    Initial commit
    here we go

---
+++ Placer/Placer/Assets/TextItem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class TextItem : MonoBehaviour {
+    public int page;
+    public string storedValue = "";
+    public Sprite storedImage;
+	// Use this for initialization
+	void Start () {
+        page = (int)FindObjectOfType<Slider>().value;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Placer/Placer/Assets/TextManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+public class TextManager : MonoBehaviour {
+    public List<Sprite> images;
+    public Image myImage;
+    public Slider slider;
+
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void SliderChanged()
+    {
+        //Deactivate all UI items
+        //Activate the items on the current slider page
+        int sliderVal = (int)slider.value;
+        myImage.sprite = images[sliderVal];
+
+        var items = FindObjectsOfType<TextItem>();
+        //Assert each item has its unique name
+
+        foreach(var i in items)
+        {
+            var itemText = i.GetComponent<Text>();
+            if (itemText != null)
+            {
+                i.storedValue = itemText.text;
+                itemText.text = "";
+
+                Debug.Log("Stored: " + i.storedValue);
+            }
+
+            //If it's an image rect
+            var img = i.GetComponent<Image>();
+            if (img!=null)
+            {
+                img.color = new Color(0, 0, 0, 0);
+            }
+        }
+
+        foreach(var i in items)
+        {
+            if (i.page == sliderVal)
+            {
+                var text = i.GetComponent<Text>();
+                if (text!=null)
+                {
+                    text.text = i.storedValue;
+                }
+
+                var IMG = i.GetComponent<Image>();
+                if (IMG != null)
+                {
+                    IMG.color = Color.black;
+                }
+            }
+        }
+    }
+}

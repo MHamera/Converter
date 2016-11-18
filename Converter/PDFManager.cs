30ea466f0f90f51b630dccf1b65df254091f5a37
Aed-Matthew <abridgedun@gmail.com>
2016-11-18 01:48:46 -0800

    Progress

--- Converter/PDFManager.cs
+++ Converter/PDFManager.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using ImageProcessor;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+
 
 using System.IO;
 
@@ -12,15 +16,23 @@ namespace Converter
     //Takes a series of images and JSON data then outputs a PDF
     class PDFManager
     {
-        private List<string> images;
+        private string[] images;
         private TextContainer textContainer;
 
         public string inputFolder = @"../../Images/";
-        public string jsonFile = "../../JSON/JSONData.json";
+        public string jsonFile = @"../../JSON/JSONData.json";
+
         public PDFManager()
         {
             LoadImages();
             LoadJSON();
+            GeneratePDF();
+        }
+
+        private void GeneratePDF()
+        {
+            var data = CreatePdf(images);
+            File.WriteAllBytes("./test.pdf", data);
         }
 
         private  void LoadJSON()
@@ -31,7 +43,27 @@ namespace Converter
 
         private void LoadImages()
         {
-            images = Directory.GetFiles(inputFolder, "*.png", SearchOption.TopDirectoryOnly).ToList();        
+            images = Directory.GetFiles(inputFolder, "*.png", SearchOption.TopDirectoryOnly);        
+        }
+
+
+        private byte[] CreatePdf(string[] bmpFilePaths)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var document = new iTextSharp.text.Document(iTextSharp.text.PageSize.EXECUTIVE., 0, 0, 0, 0);
+                iTextSharp.text.pdf.PdfWriter.GetInstance(document, ms).SetFullCompression();
+                document.Open();
+                foreach (var path in bmpFilePaths)
+                {
+                    var imgStream = File.Open(path, FileMode.Open);
+                    var image = iTextSharp.text.Image.GetInstance(imgStream);
+                    image.ScaleToFit(document.PageSize.Width, document.PageSize.Height);
+                    document.Add(image);
+                }
+                document.Close();
+                return ms.ToArray();
+            }
         }
     }
 }

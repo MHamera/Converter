a08075b00e32b63cdfadc01ae005550eb07068bb
Aed-Matthew <abridgedun@gmail.com>
2016-11-19 12:22:42 -0800

    Added commenting

--- Converter/PDFManager.cs
+++ Converter/PDFManager.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using ImageProcessor;
 using iTextSharp.text.pdf;
 using iTextSharp.text;
 
--- Converter/TextJSON.cs
+++ Converter/TextJSON.cs
@@ -16,6 +16,14 @@ namespace Converter
 
     [System.Serializable]
 
+    //A TextJSON item can appear on one or more pages
+    //"page" refers to the main page on the PDF it will show up on
+    //If it shows up on multiple pages, the secondary pages will be contained inside otherPages[]
+    //x and y indicates where the text or box start at
+    //width and height is only used for the text boxes
+    //FieldName corresponds exactly to a variable in FormSettings, acquired via reflection
+    //Finally, "text" is the value that shows up in my PDF editor, or a description. It remains unused when rendering.
+
     public class TextJSON
     {
         public int page;
@@ -29,6 +37,7 @@ namespace Converter
 
     [System.Serializable]
 
+    //This contains basic information about the PDF images
     public class MainImage
     {
         public int totalPages;
